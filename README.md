577af7e9c597be72f09790753e48eb737b1be0ad
RobertWesner <robert@wesner.io>
2024-08-21 20:09:26 +0200

    Prevent caching

--- README.md
+++ README.md
@@ -11,15 +11,15 @@ Wish, I knew...
 
 <table><tr><th width="10000">YouTube-Play-All</th><th width="10000">YouTube-Shadow-Comment</th><th width="10000">YouTube-O-Clock</th></tr><tr><td><div align="center">
     <a href="https://github.com/RobertWesner/YouTube-Play-All">
-        <img src="img/projects/userscripts/youtube/ytpa.png">
+        <img src="img/projects/userscripts/youtube/ytpa.png#1724263666">
     </a>
 </div></td><td><div align="center">
     <a href="https://github.com/RobertWesner/YouTube-Shadow-Comment">
-        <img src="img/projects/userscripts/youtube/ytsc.png">
+        <img src="img/projects/userscripts/youtube/ytsc.png#1724263666">
     </a>
 </div></td><td><div align="center">
     <a href="https://github.com/RobertWesner/YouTube-O-Clock">
-        <img src="img/projects/userscripts/youtube/ytoc.png">
+        <img src="img/projects/userscripts/youtube/ytoc.png#1724263666">
     </a>
 </div></td></tr></table>
 
@@ -27,7 +27,7 @@ Wish, I knew...
 
 <table><tr><th width="10000">TITRYES</th></tr><tr><td><div align="center">
     <a href="https://github.com/RobertWesner/titryes">
-        <img src="img/projects/security/titryes.png">
+        <img src="img/projects/security/titryes.png#1724263666">
     </a>
 </div></td></tr></table>
 
--- README.php
+++ README.php
@@ -2,6 +2,11 @@
 
 require __DIR__ . '/src/autoload.php';
 
+/**
+ * Prevents caching
+ */
+$hash = sprintf('#%d', time());
+
 ?>
 <!-- Yep, this README is completely over the top -->
 
@@ -16,24 +21,24 @@ Wish, I knew...
 <?= Table::build(
     ['YouTube-Play-All', 'YouTube-Shadow-Comment', 'YouTube-O-Clock'], [
         [
-            <<<'HTML'
+            <<<HTML
             <div align="center">
                 <a href="https://github.com/RobertWesner/YouTube-Play-All">
-                    <img src="img/projects/userscripts/youtube/ytpa.png">
+                    <img src="img/projects/userscripts/youtube/ytpa.png$hash">
                 </a>
             </div>
             HTML,
-            <<<'HTML'
+            <<<HTML
             <div align="center">
                 <a href="https://github.com/RobertWesner/YouTube-Shadow-Comment">
-                    <img src="img/projects/userscripts/youtube/ytsc.png">
+                    <img src="img/projects/userscripts/youtube/ytsc.png$hash">
                 </a>
             </div>
             HTML,
-            <<<'HTML'
+            <<<HTML
             <div align="center">
                 <a href="https://github.com/RobertWesner/YouTube-O-Clock">
-                    <img src="img/projects/userscripts/youtube/ytoc.png">
+                    <img src="img/projects/userscripts/youtube/ytoc.png$hash">
                 </a>
             </div>
             HTML,
@@ -47,10 +52,10 @@ Wish, I knew...
 <?= Table::build(
     ['TITRYES'], [
     [
-        <<<'HTML'
+        <<<HTML
         <div align="center">
             <a href="https://github.com/RobertWesner/titryes">
-                <img src="img/projects/security/titryes.png">
+                <img src="img/projects/security/titryes.png$hash">
             </a>
         </div>
         HTML,
