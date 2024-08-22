00a3910a1aea3c9ba1b697ead2d04ae16a090fed
RobertWesner <robert@wesner.io>
2024-08-22 18:46:57 +0200

    Absolute overkill

--- README.php
+++ README.php
@@ -10,38 +10,35 @@ $hash = sprintf('?%d', time());
 ?>
 <!-- Yep, this README is completely over the top -->
 
+<!--
 ## Who am I?
 
 Wish, I knew...
+-->
 
 ## Projects
 
 ### Userscripts
 
 <?= Table::build(
-    ['YouTube-Play-All', 'YouTube-Shadow-Comment', 'YouTube-O-Clock'], [
+    ['YouTube-Play-All', 'YouTube-Shadow-Comment', 'YouTube-O-Clock'],
+    [
         [
-            <<<HTML
+            sprintf(<<<HTML
             <div align="center">
-                <a href="https://github.com/RobertWesner/YouTube-Play-All">
-                    <img src="img/projects/userscripts/youtube/ytpa.png$hash">
-                </a>
+                <a href="https://github.com/RobertWesner/YouTube-Play-All">%s</a>
             </div>
-            HTML,
-            <<<HTML
+            HTML, Image::build('/img/projects/userscripts/youtube/ytpa.png')),
+            sprintf(<<<HTML
             <div align="center">
-                <a href="https://github.com/RobertWesner/YouTube-Shadow-Comment">
-                    <img src="img/projects/userscripts/youtube/ytsc.png$hash">
-                </a>
+                <a href="https://github.com/RobertWesner/YouTube-Shadow-Comment">%s</a>
             </div>
-            HTML,
-            <<<HTML
+            HTML, Image::build('/img/projects/userscripts/youtube/ytsc.png')),
+            sprintf(<<<HTML
             <div align="center">
-                <a href="https://github.com/RobertWesner/YouTube-O-Clock">
-                    <img src="img/projects/userscripts/youtube/ytoc.png$hash">
-                </a>
+                <a href="https://github.com/RobertWesner/YouTube-O-Clock">%s</a>
             </div>
-            HTML,
+            HTML, Image::build('/img/projects/userscripts/youtube/ytoc.png')),
         ],
     ],
 ) ?>
@@ -50,19 +47,41 @@ Wish, I knew...
 ### Security
 
 <?= Table::build(
-    ['TITRYES'], [
+    ['TITRYES', ''],
     [
-        <<<HTML
-        <div align="center">
-            <a href="https://github.com/RobertWesner/titryes">
-                <img src="img/projects/security/titryes.png$hash">
-            </a>
-        </div>
-        HTML,
+        [
+            sprintf(<<<HTML
+            <div align="center">
+                <a href="https://github.com/RobertWesner/titryes">%s</a>
+            </div>
+            HTML, Image::build('/img/projects/security/titryes.png')),
+        ],
     ],
-],
 ) ?>
 
 
 ### Other
 
+
+<?= Table::build(
+    ['AWDY', ''],
+    [
+        [
+            sprintf(
+                <<<HTML
+                <div align="center">
+                    <a href="https://github.com/RobertWesner/awdy">%s</a>
+                </div>
+                HTML,
+                Image::build(
+                    '/img/projects/other/awdy_rounded.gif',
+                    'Are We Done Yet? Spice up your PHP-scripts with progress-bars and more! ',
+                )
+            ),
+        ],
+    ],
+) ?>
+
+
+<br>
+<img src="/img/schmvs.png" alt="/ᐠ｡ꞈ｡ᐟ\">
--- build/render-images.sh
+++ build/render-images.sh
@@ -10,8 +10,9 @@ ROUND=30
     magick convert -size "${WIDTH}x${HEIGHT}" xc:none -draw "roundrectangle 0,0,$WIDTH,$HEIGHT,$ROUND,$ROUND" mask.png
     for filename in $(find img -name '*.html'); do
         image=${filename%.*}.png
-        chromium --headless ---chrome-frame --window-size=$WIDTH,$((HEIGHT + 200)) --disable-gpu --screenshot="$image" $filename
+        chromium --headless ---chrome-frame --window-size=$WIDTH,$((HEIGHT + 200)) --disable-gpu --screenshot="$image" --timeout=1000 $filename
         mogrify -crop "${WIDTH}x${HEIGHT}+0+0" $image
         magick convert $image -matte mask.png -compose DstIn -composite $image
+        mogrify -strip $image
     done
 )
